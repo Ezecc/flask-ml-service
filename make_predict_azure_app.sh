f4c82b26692f1a665a2b0e53621fde086b5b509b
ezecc <cc.eze90@gmail.com>
2022-09-11 11:52:55 +0200

    changes python version

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://mywebapp-123456.azurewebsites.net:$PORT/predict 
+     -X POST https://mywebapp654321.azurewebsites.net:$PORT/predict 
      # TODO: Replace the <yourappname> with your application name 
