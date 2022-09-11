592c0f82a29e1c73c15befaa53f55b46c7439ece
ezecc <cc.eze90@gmail.com>
2022-09-11 13:03:55 +0200

    modifies the scripts

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -46,5 +46,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://mywebapp654321.azurewebsites.net:$PORT/predict
+     -X POST https://azurecicdpipeline.azurewebsites.net:$PORT/predict
      #your application name <yourappname>goes here
